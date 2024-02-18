8e6801676f728288e174534c492f6fbf9733c35a
chapsjust <2238424@carrefour.cegepvicto.ca>
2024-02-17 21:16:15 -0500

    all

--- src/controllers/utilisateursController.js
+++ src/controllers/utilisateursController.js
@@ -1,29 +1,36 @@
-const utilisateursModel = require('../models/utilisateursModel');
+const utilisateursModel = require("../models/utilisateursModel");
 
 const ajouterUtilisateur = async (req, res) => {
-    const { nom, courriel, mot_de_passe } = req.body;
+  const { nom, courriel, mot_de_passe } = req.body;
 
-    if (!nom || !courriel || !mot_de_passe) {
-        return res.status(400).json({ erreur: 'Un ou plusieurs champs obligatoires sont manquants' });
+  if (!nom || !courriel || !mot_de_passe) {
+    return res
+      .status(400)
+      .json({ erreur: "Un ou plusieurs champs obligatoires sont manquants" });
+  }
+
+  try {
+    const emailExists = await utilisateursModel.validerCourriel(courriel);
+    if (emailExists) {
+      return res.status(400).json({ erreur: "Le courriel existe déjà" });
     }
 
-    utilisateursModel.validerCourriel(courriel)
-    .then((result) =>
-    {
-        if (result) {
-            return res.status(400).json({ erreur: 'Le courriel existe déjà' });
-        }
-    })
-
-    utilisateursModel.ajouterUtilisateur(nom, courriel, mot_de_passe, (error, result) => {
-        if (error) {
-            console.error(error); 
-            return res.status(500).json({ erreur: "Erreur lors de l'ajout de l'utilisateur" });
-        }
-        res.status(201).json({ message: "Utilisateur ajouté avec succès", nom, courriel, cle_api: result.insertId});
-    });
+    const cle_api = await utilisateursModel.ajouterUtilisateur(
+      nom,
+      courriel,
+      mot_de_passe
+    );
+    res
+      .status(201)
+      .json({ message: "Utilisateur ajouté avec succès", cle_api });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ erreur: "Erreur lors de l'ajout de l'utilisateur" });
+  }
 };
 
 module.exports = {
-    ajouterUtilisateur
+  ajouterUtilisateur,
 };
